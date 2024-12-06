14ad443cbee2588544395cc31ee767462124b6ed
cyberdeeb <abrahamdeeb@gmail.com>
2024-12-06 10:50:22 -0800

    Adding echo function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import sys
 
+def echo(user_input):
+    """Handles the 'echo' command."""
+    print(' '.join(user_input.split(' ')[1:]))
 
 def main():
+
     while True:
         sys.stdout.write("$ ")
 
@@ -10,9 +14,11 @@ def main():
 
         if command == 'exit 0':
             sys.exit()
-            
-
-        print(f'{command}: command not found')
+        
+        if command.startswith('echo'):
+            echo(command)
+        else:
+            print(f'{command}: command not found')
 
 
 if __name__ == "__main__":
